234b13af2e24c5b6caf1f7aa4e2dceba80cc6dc3
Derrick Fox <HomeFolder@Derricks-Mac-mini.local>
2014-10-31 15:16:32 -0400

    Initial Commit

---
+++ MultiLineInput/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MultiLineInput
+//
+//  Created by Derrick Fox on 10/31/14.
+//  Copyright (c) 2014 Derrick Fox. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
