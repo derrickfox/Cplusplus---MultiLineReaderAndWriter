c9ac78096b0a7a2b59c69c19daf823f2b7b0d465
derrickfox <fox.derrick@gmail.com>
2015-06-12 15:09:15 -0400

    Initial Commit

--- MultiLineInput/main.cpp
+++ MultiLineInput/main.cpp
@@ -1,15 +1,36 @@
-//
-//  main.cpp
-//  MultiLineInput
-//
-//  Created by Derrick Fox on 10/31/14.
-//  Copyright (c) 2014 Derrick Fox. All rights reserved.
-//
-
+#include <fstream>
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
+    string word1, word2, word3, word4;
+    
+    ifstream inFile;
+    ofstream outFile;
+    
+    inFile.open("Input.txt");
+    outFile.open("Output.txt");
+    
+    /*
+    cout << "Enter word 1 " << endl;
+    cin >> word1;
+    cout << "Enter word 2 " << endl;
+    cin >> word2;
+    cout << "Enter word 3 " << endl;
+    cin >> word3;
+    cout << "Enter word 4 " << endl;
+    cin >> word4;
+    */
+    
+    while (!inFile.eof()) {
+        inFile >> word1 >> word2 >> word3 >> word4;
+        cout << word1 << " " << word2 << " " << word3 << " " << word4 << endl;
+        outFile << word1 << " " << word2 << " " << word3 << " " << word4 << endl;
+    }
+    
+    
+    
+    
     return 0;
 }
